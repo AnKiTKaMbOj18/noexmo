334a22d66c04142fc7d156dd751dc93efcfef5fe
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-06-23 23:55:11 +0200

    noexmo | initial setup | reading command line args | using yargs package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,9 @@
-const getNotes = require("./notes.js");
 const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
+
+const getNotes = require("./notes.js");
+
 
 const notes = getNotes();
 console.log(notes);
@@ -8,10 +11,59 @@ console.log(notes);
 console.log(validator.default.isURL("http://gmail.com"));
 console.log(chalk.blue("Using chalk"));
 
+const command = process.argv[2];
+
+// customize yargs version
+yargs.version("1.1.0");
+
+// Creating add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note!");
+  }
+});
+
+// Creating remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function() {
+    console.log("Removing the note!");
+  }
+});
+
+// Creating list command
+yargs.command({
+  command: "list",
+  describe: "To list all notes",
+  handler: function() {
+    console.log("Listing all notes!");
+  }
+});
+
+// Creating read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading the note!");
+  }
+});
+
+// to create yargs custom commands
+// add, remove, read, list
+
+console.log(process.argv); // to read the command line arguments ex: node app.js Ankit
+console.log(yargs.argv);
 
 
-console.log(chalk);
-
+if(command === "Add") {
+  console.log("Adding note!!");
+} else if (command === "Remove") {
+  console.log("Removing note!!");
+}
 
 
 
