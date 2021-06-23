4114789fbb3409a9564654b416ff2f3684928069
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-06-23 23:18:37 +0200

    noexmo | initial changes | added nodemon & other packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,36 @@
-const fs = require("fs");
+const getNotes = require("./notes.js");
+const validator = require("validator");
+const chalk = require("chalk");
 
-// fs.writeFileSync("notes.txt","this file was created by Node.js!")
+const notes = getNotes();
+console.log(notes);
 
-fs.appendFileSync("notes.txt","\nthis is appended text")
+console.log(validator.default.isURL("http://gmail.com"));
+console.log(chalk.blue("Using chalk"));
+
+
+
+console.log(chalk);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const name = require("./utils.js");
+// const add = require("./utils.js");
+// const sum = add (4,-2);
+// console.log(sum);
+
+
+
+// const fs = require("fs");
+//  fs.writeFileSync("notes.txt","this file was created by Node.js!")
+// fs.appendFileSync("notes.txt","\nthis is appended text")
