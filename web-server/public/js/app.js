f6fba3eca1a8e71a927d5b2c9864aac31e550827
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-01 00:06:11 +0200

    noexmo | changes for weather app | API endpoints setup | browser app setup | other changes

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log("client side javascript file is loaded!");
+
+// fetch("http://localhost:3000/test").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+fetch("http://localhost:3000/weather?address=Amsterdam").then((response)=>{
+  response.json().then((resp)=>{
+    if(resp.error) {
+      console.log("Error: ", resp.error);
+    } else {
+      console.log(resp);
+    }
+  })
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
+const { geoCode } = require("./utils/geocode");
+const { forecast } = require("./utils/forecast");
+const { testApi } = require("./utils/testApi");
 
 const app = express();
 
@@ -8,19 +12,29 @@ const app = express();
 
 // define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location.
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
 
-app.get("/hbs", (req, res) => {
+// to resolve cors issue for browser call
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+
+app.get("/home", (req, res) => {
   res.render("index", {
     title: "Weather App",
     description: "Home page for weather app",
+    footerText: "Home Footer",
+    createdBy: "Ankit Kamboj",
   });
 });
 
@@ -28,6 +42,8 @@ app.get("/about", (req, res) => {
   res.render("about", {
     title: "About me",
     description: "About Page for weather app",
+    footerText: "About Footer",
+    createdBy: "Ankit Kamboj",
   });
 });
 
@@ -35,6 +51,8 @@ app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help page",
     description: "Help page content!",
+    footerText: "Help Footer",
+    createdBy: "Ankit Kamboj",
   });
 });
 
@@ -53,8 +71,87 @@ app.get("/help", (req, res) => {
 //   });
 // });
 
-app.get("/contact", (req, res) => {
-  res.send("Contact page!");
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a address!",
+    });
+  }
+  geoCode(req.query.address, (error, data) => {
+    if (error) {
+      return res.send({
+        error,
+      });
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error
+        });
+      }
+      // console.log(data.location);
+      // console.log(forecastData);
+      return res.send({
+        location: data.location,
+        forecast: forecastData,
+        address: req.query.address,
+      })
+    });
+  });
+  // res.send({
+  //   address: req.query.address,
+  //   forecast: "",
+  //   location: "",
+  // });
+});
+
+app.get("/test",(req,res)=>{
+  testApi((error, data)=>{
+    if(error) {
+      res.send({
+        error: error,
+      })
+    } else {
+      res.send({
+        data
+      })
+    }
+  })
+})
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term!",
+    });
+  }
+  // console.log(req);
+  res.send({
+    products: [],
+    search: req.query.search,
+    rating: req.query.rating,
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  // res.send("Help page content not found!");
+  res.render("404", {
+    title: "Not found!",
+    errorMessage: "Help article not found",
+    footerText: "Not found page footer",
+    createdBy: "Ankit Kamboj",
+  });
+});
+
+app.get("*", (req, res) => {
+  // res.send("My 404 page!");
+  res.render("404", {
+    title: "Not found!",
+    errorMessage: "Page not found",
+    footerText: "Not found page footer",
+    createdBy: "Ankit Kamboj",
+  });
 });
 
 app.listen(3000, () => {
