14b358f9cb66f88e4d825bbec3dc432766b4aa4e
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-06-30 20:50:17 +0200

    noexmo | setup hbs template for express | added app routes using hbs template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,13 +6,36 @@ const app = express();
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
 
+// define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
+// Setup handlebars engine and views location.
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get("/hbs", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Weather App",
+    description: "Home page for weather app",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    description: "About Page for weather app",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    description: "Help page content!",
+  });
 });
 
 // app.get("", (req, res) => {
