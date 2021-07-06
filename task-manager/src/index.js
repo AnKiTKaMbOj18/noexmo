1665d2812a88a02d89ab0301e1c68b38286d6303
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-06 22:14:23 +0200

    noexmo | TM changes for getting users list and user by id

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,6 +22,30 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params);
+  User.findById({ _id: req.params.id })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
 app.post("/tasks", (req, res) => {
   console.log(req.body);
   const task = new Task(req.body);
