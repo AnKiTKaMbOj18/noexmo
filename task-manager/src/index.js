5dad0e464441bf5cb09be012cb4231c1df192226
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-10 13:32:13 +0200

    noexmo | setup express middleware | testing the setup

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,8 +3,26 @@ require("./db/mongoose");
 
 const userRouter = require("./routers/userRouter");
 const taskRouter = require("./routers/taskRouter");
+
 const app = express();
 const port = process.env.PORT || 3000;
+// middleware
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    res.send("GET request are disabled!");
+  } else {
+    next();
+  }
+  // console.log(req.method, req.path);
+  // next();
+});
+
+// middleware for maintainance
+// app.use((req, res, next) => {
+//     res.status(503).send("Website is under maintainance, please try after sometime!");
+//   // console.log(req.method, req.path);
+//   // next();
+// });
 
 // app.use(cors());
 app.use(express.json());
@@ -12,10 +30,13 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+// without middleware : new request -> new route handler
+//
+// with middleware: new request -> do something -> run new handler
+
 app.listen(port, () => {
   console.log("Server is up n running on port " + port);
 });
-
 
 // testing jwt token functionality
 // const jwt = require("jsonwebtoken");
