8ddb3ba428e840e4bae11f38a88e2827c5192b32
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-11 13:58:45 +0200

    noexmo | implementing cascasding for tasks to be deleted if owner user is deleted | userschema remove method for cascading tasks | added env variable for auth sec key

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -72,7 +73,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
 
-  const token = jwt.sign({ _id: user._id.toString() }, "thisismysecret");
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.AUTH_SECRET_KEY);
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
@@ -101,6 +102,15 @@ userSchema.pre("save", async function (next) {
   }
   // console.log("from user schema middleware!");
 
+  next();
+});
+
+// delete user tasks when user is removed
+// implementing cascade for deleting all tasks
+// that belong to user to be deleted.
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
