f23f1ea39b920532b2f8a47f67ce062d8242e969
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-12 22:48:05 +0200

    noexmo | added user endpoints to upload image delete image and get image

--- task-manager/src/routers/userRouter.js
+++ task-manager/src/routers/userRouter.js
@@ -120,6 +120,20 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
 router.delete(
   "/users/me/avatar",
   auth,
@@ -127,6 +141,27 @@ router.delete(
     req.user.avatar = undefined;
     await req.user.save();
     res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.get(
+  "/users/:id/avatar",
+  auth,
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      // const avatar = await req.user.save();
+      if(!user && !user.avatar) {
+        throw new Error("");
+      }
+      res.set("Content-Type","image/jpg");
+      res.send(user.avatar);
+    } catch (error) {
+      res.status(404).send();
+    }
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
@@ -212,3 +247,4 @@ module.exports = router;
 //     res.status(500).send(error);
 //   }
 // });
+
