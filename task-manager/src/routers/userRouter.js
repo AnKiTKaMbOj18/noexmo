ea81f1bb57b8a27964079d03de44a2681d92ea9d
ankitkamboj18 <ankitkamboj124@gmail.com>
2021-07-12 22:31:20 +0200

    noexmo | endpoint to add images to user profile | delete user avatar endpoint

--- task-manager/src/routers/userRouter.js
+++ task-manager/src/routers/userRouter.js
@@ -7,7 +7,7 @@ const taskRouter = require("./taskRouter");
 const router = new express.Router();
 
 const upload = multer({
-  dest: "avatars",
+  // dest: "avatars",
   limits: {
     fileSize: 1000000,
   },
@@ -77,17 +77,6 @@ router.post("/users/logoutAll", auth, async (req, res) => {
   }
 });
 
-router.post(
-  "/users/me/avatar",
-  upload.single("avatar"),
-  (req, res) => {
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-  }
-);
-
 router.get("/users/me", auth, async (req, res) => {
   try {
     // const users = await User.find({});
@@ -129,6 +118,20 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+
+router.delete(
+  "/users/me/avatar",
+  auth,
+  async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
 
